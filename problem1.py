6a6d5a9ae5448188a1bf57fcc13b8dbe3c1c0c14
jawang35 <jawang35@gmail.com>
2016-10-15 11:38:03 -0700

    Changed comment styles to multiline and added hand computation for Problem 1

--- problem1.py
+++ problem1.py
@@ -1,9 +1,20 @@
-# Problem 1 - Multiples of 3 and 5
+'''
+Problem 1 - Multiples of 3 and 5
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-# The sum of these multiples is 23.
-#
-# Find the sum of all the multiples of 3 or 5 below 1000.
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+'''
+Computed by Hand:
+(3 + 6 + ... + 999) + (5 + 10 + ... + 995) - (15 + 30 + ... + 990)
+= 3 * (1 + 2 + ... + 333) + 5 * (1 + 2 + ... + 199) - 15 * (1 + 2 + ... + 66)
+= (3 * 333 * 334 / 2) + (5 * 199 * 200 / 2) - (15 * 66 * 67 / 2)
+= 166833 + 99500 - 33165
+= 233168
+'''
 
 from helpers import runtime
 
--- problem3.py
+++ problem3.py
@@ -1,8 +1,10 @@
-# Problem 3 - Largest Prime Factor
+'''
+Problem 3 - Largest Prime Factor
 
-# The prime factors of 13195 are 5, 7, 13 and 29.
-#
-# What is the largest prime factor of the number 600851475143 ?
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+'''
 
 import math
 from helpers import runtime
