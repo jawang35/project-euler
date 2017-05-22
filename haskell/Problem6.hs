122bdd14f324d264f1fa65c543f4921c9eb60726
jawang35 <jawang35@gmail.com>
2017-05-21 23:19:19 -0700

    haskell - Problem 6

---
+++ haskell/Problem6.hs
@@ -0,0 +1,24 @@
+{- |
+Problem 6 - Sum Square Difference
+
+The sum of the squares of the first ten natural numbers is,
+
+1**2 + 2**2 + ... + 10**2 = 385
+
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)**2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+-}
+
+sumSquareDifference :: Integer -> Integer
+sumSquareDifference maximum =
+    squareOfSums - sumOfSquares
+    where naturalNumbers = [1..maximum]
+          squareOfSums = (sum naturalNumbers) ^ 2
+          sumOfSquares = sum $ map (^2) naturalNumbers
