d2129077b3ce9fff898dd3d031b94f9389393844
jawang35 <jawang35@gmail.com>
2018-06-15 00:13:10 -0700

    haskell - Problem 18 and 67

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -15,6 +15,8 @@ import Problem14
 import Problem15
 import Problem16
 import Problem17
+import Problem18
+import Problem67
 
 problem1 = TestCase $ assertEqual "Answer" 233168 Problem1.answer
 problem2 = TestCase $ assertEqual "Answer" 4613732 Problem2.answer
@@ -34,6 +36,10 @@ problem14 = TestCase $ assertEqual "Answer" 837799 Problem14.answer
 problem15 = TestCase $ assertEqual "Answer" 137846528820 Problem15.answer
 problem16 = TestCase $ assertEqual "Answer" 1366 Problem16.answer
 problem17 = TestCase $ assertEqual "Answer" 21124 Problem17.answer
+problem18 = TestCase (do answer <- Problem18.answer
+                         assertEqual "Answer" 1074 answer)
+problem67 = TestCase (do answer <- Problem67.answer
+                         assertEqual "Answer" 7273 answer)
 
 tests = TestList
     [ TestLabel "Problem 1" problem1
@@ -51,6 +57,8 @@ tests = TestList
     , TestLabel "Problem 15" problem15
     , TestLabel "Problem 16" problem16
     , TestLabel "Problem 17" problem17
+    , TestLabel "Problem 18" problem18
+    , TestLabel "Problem 67" problem67
     ]
 
 main :: IO Counts
