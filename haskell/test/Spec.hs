baa18ea13d81dabe887e291149a2a76adcb0e7c6
jawang35 <jwang@surfline.com>
2018-07-23 11:14:07 -0700

    haskell - Problem 53

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -50,6 +50,7 @@ import Problem48
 import Problem49
 import Problem50
 import Problem52
+import Problem53
 import Problem67
 
 problem1 = TestCase $ assertEqual "Answer" 233168 Problem1.answer
@@ -108,6 +109,7 @@ problem48 = TestCase $ assertEqual "Answer" 9110846700 Problem48.answer
 problem49 = TestCase $ assertEqual "Answer" 296962999629 Problem49.answer
 problem50 = TestCase $ assertEqual "Answer" 997651 Problem50.answer
 problem52 = TestCase $ assertEqual "Answer" 142857 Problem52.answer
+problem53 = TestCase $ assertEqual "Answer" 4075 Problem53.answer
 problem67 = TestCase (do answer <- Problem67.answer
                          assertEqual "Answer" 7273 answer)
 
@@ -162,6 +164,7 @@ tests = TestList
     , TestLabel "Problem 49" problem49
     , TestLabel "Problem 50" problem50
     , TestLabel "Problem 52" problem52
+    , TestLabel "Problem 53" problem53
     , TestLabel "Problem 67" problem67
     ]
 
